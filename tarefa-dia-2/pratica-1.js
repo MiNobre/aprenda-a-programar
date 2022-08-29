0fcf45d0abe9f0a875508aeb536d014786a16487
MiNobre <michellenobremelo@gmail.com>
2022-08-28 22:29:27 -0300

    pratica-1-dados

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+let nome = "Michelle" 
+console.log(nome)
+let email = "michellenobremelo@gmail.com" 
+console.log(email)
+let profissão = "marketing" 
+console.log(profissão)
+let idade = "21"
+console.log(idade)
+let viagem = "grécia"
+console.log (viagem)
+
+
